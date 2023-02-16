af386832377e5b04b044793202c6699b4f8ae2aa
bozya02 <bozinandrey1@yandex.ru>
2023-02-16 19:26:43 +0300

    Add project files.

---
+++ Breakdown/DB/BreakdownEntities.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Breakdown.DB
+{
+    public partial class BreakdownEntities
+    {
+        private static BreakdownEntities _context;
+
+        public static BreakdownEntities GetContext()
+        {
+            if (_context == null)
+                _context = new BreakdownEntities();
+
+            return _context;
+        }
+    }
+}
---
+++ Breakdown/DB/DataAccess.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Breakdown.DB
+{
+    public class DataAccess
+    {
+        public delegate void AddNewItem();
+        public event AddNewItem AddNewItemEvent;
+
+        private static DbSet<Client> _clients => BreakdownEntities.GetContext().Clients;
+    }
+}
